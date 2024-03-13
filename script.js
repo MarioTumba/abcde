fb8dfb59091b66de0533d17cfcb37b64dddc4b2c
MarioTumba <mario.tumba@unmsm.edu.pe>
2024-03-12 20:03:56 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+alert("Hola este es mi Javascript");
+
+
+const nombre = "Mario";
+
+console.log(nombre);
+
+let contenidoTitulo = "Nombre";
+let titulo = document.querySelector(".top");
+titulo.innerHTML = contenidoTitulo;
+
+
+if(textoTitulo == "Nombre") {
+    titulo.innerHTML
+}
